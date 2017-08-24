585e0155ec9314ba58475b7154513233ac957ab4
Robert Johansson <robertjo79@gmail.com>
2017-08-24 17:10:55 +0200

    Fix for BLOB/TEXT column 'params' used in key specification without a key length - in migration

--- db/migrate/20151204000125_add_rails_lti2_provider_registration_correlation_id.rb
+++ db/migrate/20151204000125_add_rails_lti2_provider_registration_correlation_id.rb
@@ -1,6 +1,6 @@
 class AddRailsLti2ProviderRegistrationCorrelationId < ActiveRecord::Migration
   def change
     add_column :rails_lti2_provider_registrations, :correlation_id, :text
-    add_index :rails_lti2_provider_registrations, :correlation_id, :unique => true
+    add_index :rails_lti2_provider_registrations, :correlation_id, :unique => true, :length => {:correlation_id => 255 }
   end
 end
